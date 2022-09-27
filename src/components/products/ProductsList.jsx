37cb245c40ab6e6272fcc36bc14717488e443268
Brayan <braian_bg@hotmail.es>
2022-09-27 20:45:50 +0200

    Botones para ver todos lo productos o mas productos

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -1,43 +1,47 @@
-import { useNavigate, useParams } from "react-router-dom";
-import Homepage from "../../routes/Homepage";
-import useProducts from "../../shared/hooks/useProducts";
-import ButtonTo from "../ButtonTo";
-import ErrorMessage from "../ErrorMessage";
-import Loading from "../Loading";
-import Modal from "../Modal/Modal";
-import { Products } from "./Products";
+import { useNavigate, useParams } from 'react-router-dom'
+import useProducts from '../../shared/hooks/useProducts'
+import ButtonTo from '../ButtonTo'
+import ErrorMessage from '../ErrorMessage'
+import Loading from '../Loading'
+import Modal from '../Modal/Modal'
+import { Products } from './Products'
 import '../../styles/productList.css'
 
-
 export const ProductsList = () => {
-    //Recuperara el resultado de la busqueda
-    const { products, info, loading, error } = useProducts();
-    
-    //Para navegar entre las paginas
-    const navigate = useNavigate();
+  //Recuperara el resultado de la busqueda
+  const { products, info, loading, error } = useProducts()
 
-    //Recuperar el parametro de la url con useParams
-    console.log(products);
-    console.log(info);
-    const { filter } = useParams();
-    console.log(filter);
-    if (loading) return <Loading classe={"loader__products"}/>
-    if (error) return (
+  //Para navegar entre las paginas
+  const navigate = useNavigate()
+
+  //Recuperar el parametro de la url con useParams
+  console.log(products)
+  console.log(info)
+  const { filter } = useParams()
+  console.log(filter)
+  if (loading) return <Loading classe="loader__products" />
+  if (error)
+    return (
       <Modal>
         <ErrorMessage error={error} />
-        <ButtonTo  text={'Home'} classe={'card__button'} />
+        <ButtonTo text="Home" classe="card__button" />
       </Modal>
     )
-    return (
-        <section className="productList__container">
-            <header className="producList__header">
-                <nav className="productList__header__nav">
-                    <button onClick={() => info.prevPage && navigate(info.prevPage)}>Prev</button>
-                    <h1>Products List: {filter}</h1>
-                    <button onClick={() => info.nextPage && navigate(info.nextPage)}>Next</button>
-                </nav>
-            </header>
-            <Products products={products} />
-        </section>
-    )
+  return (
+    <section className="productList__container">
+      <header className="producList__header">
+        <nav className="productList__header__nav">
+          <button onClick={() => info.prevPage && navigate(info.prevPage)}>
+            Prev
+          </button>
+          <h1>Products List: {filter}</h1>
+          <button onClick={() => info.nextPage && navigate(info.nextPage)}>
+            Next
+          </button>
+        </nav>
+      </header>
+      <Products products={products} />
+      <ButtonTo to="/products" text="Ver mas productos" />
+    </section>
+  )
 }
